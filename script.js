d47af28d4972a16f272c2403d31d847c48d19a46
chiarakulzer <c.kulzer@medienwuerfel.de>
2023-10-13 12:03:00 +0200

    Test Upload - 04.10.23

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Elements (DOM)
-const btnStart = document.getElementById("start--btn");
-const btnTeam = document.getElementById("team--btn");
-
 const sectionStart = document.getElementById("start-section");
 const sectionTeam = document.getElementById("team-section");
 const sectionCounter = document.getElementById("counter-section");
 const sectionScore = document.getElementById("score-section");
+const sectionWinner = document.getElementById("winner-section");
 
 const characters = document.querySelectorAll(".select-container");
 const transition = document.querySelector(".fade-transition");
@@ -13,9 +11,12 @@ let clock = document.getElementById("seconds");
 
 const teamNameElement0 = document.getElementById('team--name-0');
 const teamNameElement1 = document.getElementById('team--name-1');
-const scoreElements = document.querySelector('.score--value');
-const scoreElement0 = document.querySelector('.score--value-0');
-const scoreElement1 = document.querySelector('.score--value-1');
+
+let scoreValueEl0 = document.getElementById('score--value-0');
+let scoreValueEl1 = document.getElementById('score--value-1');
+
+let scores = document.querySelectorAll(`.values`);
+
 
 var characterArray = {
     character1: {
@@ -51,37 +52,56 @@ var characterArray = {
 };
 
 
-
-
 // Starting Condition
 let scoreValue0 = 0;
-scoreElement0.textContent = scoreValue0.toString();
+scoreValueEl0.textContent = scoreValue0.toString();
 let scoreValue1 = 0;
-scoreElement1.textContent = scoreValue1.toString();
+scoreValueEl1.textContent = scoreValue1.toString();
 
 let selectedCharacters = [];
 let teamNames = [];
-const teamImages = [];
-
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let teamImages = [];
 let playing = true;
 
 
+// GENERATE HTML
+let generatedHtml = Object.keys(characterArray).reduce(
+    (accum, currKey) =>
+        accum +
+        `<div id="${characterArray[currKey].id}" class="character" data-name="${characterArray[currKey].id}" rel="${characterArray[currKey].name}" onclick="${characterArray[currKey].id}">
+    <img class="character__img" src="${characterArray[currKey].imgSrc}" />
+    <p class="character__name" ">${characterArray[currKey].name}</p>
+  </div>`,
+    ""
+);
+characters.forEach((element) => element.innerHTML = generatedHtml);
+
 // FUNCTIONS
+function sectionTransition(oldSection, newSection){
+    oldSection.classList.add("hidden");
+    newSection.classList.remove("hidden");
+}
 function selectCharacter(characterId) {
     const characterImage = document.getElementById(characterId);
     const playerId = characterId.split('-')[0];
     selectedCharacters[playerId] = characterImage.src;
     alert(`Player ${playerId} selected character: ${characterImage.alt}`);
 }
-function startGame() {
-
+function saveTeamNames(nameEl1, nameEl2){
+    let names = new Array(2);
+    names[0] = nameEl1.value;
+    names[1] = nameEl2.value;
+    return names;
 }
-function sectionTransition(oldSection, newSection){
-    oldSection.classList.add("hidden");
-    newSection.classList.remove("hidden");
+function setProfil(namesArr, imageArr){
+    //score--name score--img-container
+    const names = document.querySelectorAll(".score--name");
+    const images = document.querySelectorAll(".score--img");
+
+    for(let i = 0; i < names.length; i++){
+        names[i].innerHTML = namesArr[i];
+        images[i].src = imageArr[i];
+    }
 }
 function countdown(){
     let secondsRemaining = 3;
@@ -105,52 +125,50 @@ function countdown(){
         clearInterval(myInterval);
     }
 }
-function saveTeamNames(nameEl1, nameEl2){
-    let names = new Array(2);
-    names[0] = nameEl1.value;
-    names[1] = nameEl2.value;
-    return names;
-}
 
-function setProfil(namesArr, imageArr){
-    //score--name score--img-container
-    const names = document.querySelectorAll(".score--name");
-    const images = document.querySelectorAll(".score--img");
+function checkIfWon(score, element){
+    let elementId = element.charAt(element.length-1);
+    if(score === 10){
 
-    for(let i = 0; i < names.length; i++){
-        names[i].innerHTML = namesArr[i];
-        images[i].src = imageArr[i];
+        console.log(elementId);
+        console.log(teamNames[elementId]);
+        console.log(teamImages[elementId]);
+        document.querySelector('.winner--name').textContent = `Team ${teamNames[elementId]}`;
+        document.querySelector('.winner--img').src = teamImages[elementId];
+        sectionTransition(sectionScore, sectionWinner);
     }
 }
-// TODO: EDIT
-const switchPlayer = function () {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    for (let i = 0; i < players.length; i++) {
-        players[i].classList.toggle('player--active');
+function goal(teamid){
+    if(teamid === 0){
+        scoreValue0++;
+        document.getElementById(`score--value-${teamid}`).textContent = scoreValue0.toString();
+        checkIfWon(scoreValue0, 'scoreValue0');
+    }else if(teamid === 1){
+        scoreValue1++;
+        document.getElementById(`score--value-${teamid}`).textContent = scoreValue1.toString();
+        checkIfWon(scoreValue1, 'scoreValueEl1');
+    }else{
+        console.log('An Error occurred at line 70')
     }
-};
+}
+function resetGame(){
+    scoreValueEl0.textContent = 0;
+    scoreValueEl1.textContent = 0;
 
+    document.getElementById('team--name-0').value = '';
+    document.getElementById('team--name-1').value = '';
+    document.querySelectorAll(".character").forEach((character) => {
+        if(character.classList.contains('active')){
+            character.classList.remove('active');
+        }
+    });
 
-
-// TODO: Make "generatedHtml" to a dynamic function
-// GENERATE HTML
-let generatedHtml = Object.keys(characterArray).reduce(
-    (accum, currKey) =>
-        accum +
-        `<div id="${characterArray[currKey].id}" class="character" data-name="${characterArray[currKey].id}" rel="${characterArray[currKey].name}" onclick="${characterArray[currKey].id}">
-    <img class="character__img" src="${characterArray[currKey].imgSrc}" />
-    <p class="character__name" ">${characterArray[currKey].name}</p>
-  </div>`,
-    ""
-);
-characters.forEach((element) => element.innerHTML = generatedHtml);
-/*
-let team1 = document.querySelector('.select-container-01').length > 0;
-let team2 = document.querySelector('.select-container-02').length > 0;
- */
-
+    selectedCharacters = [];
+    teamNames = [];
+    teamImages = [];
+    playing = true;
+    sectionTransition(sectionWinner, sectionStart);
+}
 
 // CHARACTER SELECTION:
 const characterList = document.querySelectorAll(".character");
@@ -174,19 +192,13 @@ characterList.forEach((character) => {
 });
 
 
-
-
-
-// EVENTLISTENER
-
 // Start Button
-console.log(sectionCounter);
-btnStart.addEventListener("click", function () {
+document.getElementById("start--btn").addEventListener("click", function () {
     sectionTransition(sectionStart,sectionTeam);
 });
 
 // Team Button
-btnTeam.addEventListener("click", function () {
+document.getElementById("team--btn").addEventListener("click", function () {
     // Save Team Name
     teamNames = saveTeamNames(teamNameElement0, teamNameElement1);
     console.log(teamNames);
@@ -204,39 +216,30 @@ btnTeam.addEventListener("click", function () {
         transition.classList.remove("hidden");
         // Counter
         countdown();
-
         setProfil(teamNames, teamImages);
     }
 });
 
-// TODO: EDIT
+// onchange="checkFunc"
 /*
-btnHold.addEventListener('click', function () {
-    if (playing) {
-        // 1. Add current score to active players score
-        scores[activePlayer] += currentScore;
-        // 2. Check if players score is >= 100
-
-        document.getElementById(`score--${activePlayer}`).textContent =
-            scores[activePlayer];
-
-        if (scores[activePlayer] >= 20) {
-            // Finish the game
-            playing = false;
-            diceEL.classList.add('hidden');
-            document
-                .querySelector(`.player--${activePlayer}`)
-                .classList.add('player--winner');
-            document
-                .querySelector(`.player--${activePlayer}`)
-                .classList.remove('player--active');
-        } else {
-            switchPlayer();
+if (playing) {
+    scores[i].addEventListener("change", (event) => {
+        for(let i = 0; i < scores.length; i++){
+            //scores[i].textContent = event.target.value;
+            if (scores[i].textContent === 10) {
+                // Finish the game
+                playing = false;
+                scores[i] === scores[0] ? scores[1].classList.add("hidden") : scores[0].classList.add("hidden");
+                document.querySelector('.score-images').style.justifyContent = "center";
+                document.querySelector('score--btn').classList.remove('hidden');
+                document.querySelector('score-players').classList.add('hidden');
+            } else {
+            }
         }
-    }
-});
-*/
+    });
+}
 
+*/
 
 
 
